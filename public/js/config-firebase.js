03f47c35e33bb4053d340d19d499db3ab9157c88
BILSON <emmanuelbilson234@gmail.com>
2025-08-06 03:36:01 +0000

    changement de techno

---
+++ public/js/config-firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+// @ts-nocheck
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAe52fWPYRgfwmkfhT1oq17cG3brSiQxfA",
+    authDomain: "youngt-5717b.firebaseapp.com",
+    projectId: "youngt-5717b",
+    storageBucket: "youngt-5717b.firebasestorage.app",
+    messagingSenderId: "824452788058",
+    appId: "1:824452788058:web:c968088ae259c546b10d63"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
